3e99b8efe45556e3fb2776cf4821ee1f77f8472c
James Bannerman <james.bannerman@indexexchange.com>
2017-12-05 15:06:20 -0500

    lol python

---
+++ 5/5-1.py
@@ -0,0 +1,12 @@
+f1 = open('input.txt', 'r')
+
+instructions = [int(i) for i in f1.readlines()]
+steps = 0
+index = 0
+
+while index < len(instructions) and index >= 0:
+	move = instructions[index]
+	instructions[index] += 1
+	index += move
+	steps += 1
+print steps
